8c7f50e83903999cafa205b46c26ff52e6bf74c1
mrAnderson2159 <there.is.no.spoon.2159@gmail.com>
2024-10-28 17:23:18 +0700

    rename background-image zoolab-image

--- zoolab-frontend/src/components/MainLayout.js
+++ zoolab-frontend/src/components/MainLayout.js
@@ -1,5 +1,6 @@
 // src/components/MainLayout.js
 import React from "react";
+import { useLocation } from "react-router-dom";
 import Header from "./Header";
 import "./MainLayout.css";
 import "./Offcanvas";
@@ -7,13 +8,22 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Offcanvas from "./Offcanvas";
 
 const MainLayout = ({ children, headerTitle }) => {
+    const location = useLocation();
+
+    // Verifica se il percorso inizia con "/zones/"
+    const shouldShowBackground = location.pathname.startsWith("/zones");
+
     return (
         <div className="main-layout">
             <Header headerTitle={headerTitle} />
             <Offcanvas />
-            {/* Background image e overlay */}
-            <div className="background-cover"></div>
-            <div className="overlay"></div>
+            {/* Background image e overlay mostrati solo se siamo in una pagina /zones/* */}
+            {shouldShowBackground && (
+                <>
+                    <div className="zoolab-image"></div>
+                    <div className="overlay"></div>
+                </>
+            )}
             <div className="overflow-trap"></div>
             <main className="mt-5 pt-1">{children}</main>
         </div>
